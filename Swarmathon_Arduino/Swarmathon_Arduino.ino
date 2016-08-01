2b45a7935eab45909acf1ca7766847e996a34f07
Matthew Fricke <matthew@fricke.co.uk>
2016-08-01 10:55:20 -0600

    Added C macros to define PI and converting from radians to degrees This is to support the change in topics that have gripper angles from int16 degrees to float32 radians.

--- Swarmathon_Arduino/Swarmathon_Arduino.ino
+++ Swarmathon_Arduino/Swarmathon_Arduino.ino
@@ -14,6 +14,9 @@
 #include <Odometry.h>
 #include <Ultrasound.h>
 
+// Constants
+#define PI 3.14159265358979323846
+#define RAD2DEG(radianAngle) (radianAngle * 180.0 / PI)
 
 ////////////////
 ////Settings////
@@ -153,7 +156,9 @@ void parse() {
     Serial.println(txBuffer);
   }
   else if (rxBuffer == "f") {
-    int angle = Serial.parseInt();
+    float radianAngle = Serial.parseFloat();
+    int angle = RAD2DEG(radianAngle); // Convert float radians to int degrees
+    
     if (angle == -1) {
       fingers.write(fingers.read()+1);
     }
@@ -165,7 +170,9 @@ void parse() {
     }
   }
   else if (rxBuffer == "w") {
-    int angle = Serial.parseInt();
+    float radianAngle = Serial.parseFloat();
+    int angle = RAD2DEG(radianAngle); // Convert float radians to int degrees
+    
     if (angle == -1) {
       wrist.write(wrist.read()+1);
     }
